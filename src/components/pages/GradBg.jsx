f5ef77b233d19880f343c884029e4957878bdf53
pro-dev-ryan <azizul.developer@gmail.com>
2023-04-25 01:57:46 +0600

    grad responsive working

--- src/components/pages/GradBg.jsx
+++ src/components/pages/GradBg.jsx
@@ -3,8 +3,8 @@ import laptop from "../../assets/bgGrad/laptop.png";
 import Button from "../Button";
 const GradBg = () => {
   return (
-    <div className="flex flex-col-reverse md:flex-row  justify-center items-center bg-[#f7f9fc]">
-      <div className="w-1/2 space-y-5 mx-6">
+    <div className="flex flex-col-reverse md:flex-row justify-center items-center bg-[#f7f9fc] md:g section-plate">
+      <div className="w-full md:w-1/2 space-y-5 mx-6">
         <h3 className="text-xl lg:text-3xl xl:text-4xl text-secondary font-semibold">
           Bestkit Develope Creative Solutions for Companies of All Niches
         </h3>
