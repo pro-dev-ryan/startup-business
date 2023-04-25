6a9c438c839953263c56629129ace284dbcc3d7d
pro-dev-ryan <azizul.developer@gmail.com>
2023-04-26 01:08:01 +0600

    speciality section done

--- src/components/pages/Speciality.jsx
+++ src/components/pages/Speciality.jsx
@@ -1,28 +1,83 @@
 import React from "react";
+import icon from "../../assets/cards-icon/icon1.svg";
+import icon2 from "../../assets/cards-icon/icon2.svg";
+import icon3 from "../../assets/cards-icon/icon3.svg";
+import icon4 from "../../assets/cards-icon/icon4.svg";
+import icon5 from "../../assets/cards-icon/icon5.svg";
+import icon6 from "../../assets/cards-icon/icon6.svg";
+import bg from "../../assets/cards-icon/bg.svg";
 
 const Speciality = () => {
+  const datas = [
+    {
+      icon: icon,
+      title: "Clean & Elegent Design",
+      subTitle:
+        "Lorem ipsum dolor sit amet, consectetu adipiscing elit eque dolor.",
+    },
+    {
+      icon: icon2,
+      title: "100% Responsive Layout",
+      subTitle:
+        "Lorem ipsum dolor sit amet, consectetu adipiscing elit eque dolor.",
+    },
+    {
+      icon: icon3,
+      title: "Awsome Line Icon",
+      subTitle:
+        "Lorem ipsum dolor sit amet, consectetu adipiscing elit eque dolor.",
+    },
+    {
+      icon: icon4,
+      title: "Unlimited Component Library",
+      subTitle:
+        "Lorem ipsum dolor sit amet, consectetu adipiscing elit eque dolor.",
+    },
+    {
+      icon: icon5,
+      title: "Smooth UI Interaction",
+      subTitle:
+        "Lorem ipsum dolor sit amet, consectetu adipiscing elit eque dolor.",
+    },
+    {
+      icon: icon6,
+      title: "24/7 Customer Support",
+      subTitle:
+        "Lorem ipsum dolor sit amet, consectetu adipiscing elit eque dolor.",
+    },
+  ];
   return (
     <div className="bg-[#F7F9FC]">
-      <div className="section-plate">
+      <div className="mx-6 text-center space-y-5">
         <p className="text-sm lg:text-lg text-primary font-medium">
           Our core values & principles
         </p>
         <h3 className="text-xl lg:text-3xl xl:text-4xl font-semibold text-secondary">
           What Makes Us Unique From Others
         </h3>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-2 lg:gap-5 md:grid-cols-2 lg:grid-cols-3">
           {/* Card Plate style */}
-          <div className="p-16 flex flex-col gap-4 md:gap-5 lg:gap-10 justify-start items-center">
-            <div>
-              <img src="" alt="" />
+          {datas.map((data, i) => (
+            <div
+              key={i}
+              className="p-12 bg-white flex flex-col gap-4 md:gap-5 lg:gap-5 justify-start items-center"
+            >
+              <div className="w-full relative mb-2">
+                <img className="absolute z-10" src={bg} alt="circle" />
+                <img
+                  className="relative z-20 top-[25%] left-[5%]"
+                  src={data.icon}
+                  alt="icon"
+                />
+              </div>
+              <h4 className="font-semibold text-lg lg:text-lg text-secondary">
+                {data.title}
+              </h4>
+              <p className="text-neutral text-sm lg:text-base">
+                {data.subTitle}
+              </p>
             </div>
-            <h4 className="font-semibold text-lg md:text-xl lg:text-2xl text-secondary">
-              Clean & Elegant Design
-            </h4>
-            <p className="text-neutral text-sm lg:text-lg">
-              Lorem ipsum dolor sit amet, consectetu adipiscing elit eque dolor.
-            </p>
-          </div>
+          ))}
         </div>
       </div>
     </div>
