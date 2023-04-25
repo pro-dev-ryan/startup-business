bb2335b9da9699e88bd195f7c5265ac699290ae0
pro-dev-ryan <azizul.developer@gmail.com>
2023-04-25 13:17:59 +0600

    gradient all done

--- src/components/pages/GradBg.jsx
+++ src/components/pages/GradBg.jsx
@@ -3,7 +3,7 @@ import laptop from "../../assets/bgGrad/laptop.png";
 import Button from "../Button";
 const GradBg = () => {
   return (
-    <div className="flex flex-col-reverse md:flex-row justify-center items-center bg-[#f7f9fc] md:g section-plate">
+    <div className="flex flex-col-reverse md:flex-row justify-center items-center bg-[#f7f9fc] section-plate">
       <div className="w-full md:w-1/2 space-y-5 mx-6">
         <h3 className="text-xl lg:text-3xl xl:text-4xl text-secondary font-semibold">
           Bestkit Develope Creative Solutions for Companies of All Niches
@@ -14,7 +14,7 @@ const GradBg = () => {
         </p>
         <Button text={"Get Started Now"} />
       </div>
-      <div className="md:w-1/2 md:relative grad">
+      <div className="w-full md:w-1/2 relative g">
         <img src={laptop} alt="laptop-user" />
       </div>
     </div>
